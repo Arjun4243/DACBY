5c3d33f03562fd03ef7dda46b68c486f17d8c433
Arjun <arjunsinghanya35@gmail.com>
2026-05-07 23:21:14 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+import Login from './Component/Login.jsx/Login';
 import NavbarMain from './Component/Navbar/NavbarMain';
 
 
 function App() {
   return (
     <div className="App">
+      
       <NavbarMain />
+      <Login/>
    
     </div>
   );
--- frontend/src/Component/Login.jsx/Login.jsx
+++ frontend/src/Component/Login.jsx/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './login.css'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className='registration'>
+            <img src='/image/registration.png' />
+            <h1>Registration</h1>
+            <div className='form'>
+                <div>Name: <input type='text' placeholder='Enter your name' /></div>
+                <div>Email: <input type='email' placeholder='Enter your email' /></div>
+                <div>Password: <input type='password' placeholder='Enter your password' /></div>
+                <button>Login</button>
+            </div>
+        </div>
+    )
 }
 
 export default Login

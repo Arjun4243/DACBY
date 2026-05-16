08535764bcd566ce513e32b8b3ae71bb2e840b84
Arjun <arjunsinghanya35@gmail.com>
2026-05-17 02:12:45 +0530

    other website i fatch into the iframe

--- frontend/src/Component/SingleStories/SingleStories.jsx
+++ frontend/src/Component/SingleStories/SingleStories.jsx
@@ -5,33 +5,56 @@ import { FcBookmark } from "react-icons/fc";
 import { AiFillLike } from "react-icons/ai";
 import { IoIosTime } from "react-icons/io";
 import { FaBook } from "react-icons/fa";
-import { useContext } from "react";
+import { useContext,useState } from "react";
 import { StoreContextCreated } from "../../StoreContext";
+import { useNavigate } from "react-router-dom";
+import { FaWindowClose } from "react-icons/fa";
+
+
 
 
 
 function SingleStories() {
     const { storiesdata,singleStory,handleSingleStory } = useContext(StoreContextCreated);
 
+    const navigate = useNavigate();
+
+      const [expanded, setExpanded] = useState(false);
+
+
+  const handleToggleHeight = () => {
+    setExpanded(!expanded);
+  };
     return (
         <>
 
-            <div  className="container-0">
+            <div  className={`container-0 ${expanded ? "expanded" : ""}`}onClick={()=>(handleToggleHeight())}>
 
                 <div className="container-1">
+                    <FaWindowClose size={28} id="close-button" onClick={()=>(navigate(-1))} />
                     <div className="container-2">
                         <h3>{singleStory.title}</h3>
+                    
                         <p>
                             <FaEarthAfrica className="earth-icon" />
                             {singleStory.url}
                         </p>
                     </div>
+                    
                     <div className="bookmark-icon">
+                        
                         <FaRegBookmark size={28} />
                         <FcBookmark size={40} />
                     </div>
+                    
                 </div>
-
+                <iframe
+                src={singleStory.url}
+                title="Story Website"
+        title="Story Website"
+        height="500px"
+        width="100%"
+                ></iframe>
                 <hr />
 
                 <div className="container-3">

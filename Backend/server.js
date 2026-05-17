5981fd9e7249e1e6ef474915f581f36517b5e4c0
Arjun <arjunsinghanya35@gmail.com>
2026-05-17 22:36:39 +0530

    frontend allow to access

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT
 
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'https://dacby-frontend.onrender.com'],
     methods: ["GET", "POST", "PUT", "DELETE"], 
     credentials: true  
 }))

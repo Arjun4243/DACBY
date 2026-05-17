4dbec67db1a0551fd590302051af05a747d61907
Arjun <arjunsinghanya35@gmail.com>
2026-05-17 22:06:44 +0530

    authenditacion problem solve deployment

--- Backend/middleware/authMiddleWare.js
+++ Backend/middleware/authMiddleWare.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import jwt from 'jsonwebtoken'
 import userModel from '../model/userModel.js'
 
-const authMiddleWare = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
     
     const authHeader  = req.headers['authorization']
 
@@ -18,13 +18,16 @@ const authMiddleWare = (req, res, next) => {
             try{
                 const token = authHeader.split(" ")[1] //this line convert string to array that i sperated with the help of space and i give access only 1 elment no the 0
                 const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
-
                 req.userId=decoded.userId
-
                 next();
             }
             catch(error){
                 console.log(error)
+                res.status(401).json({
+                    NotificationShow: true,
+                    Headline: "Unauthorized",
+                    Message: "Invalid or expired session. Please login again."
+                });
             }
              
         
@@ -32,4 +35,4 @@ const authMiddleWare = (req, res, next) => {
 
 }
 
-export default authMiddleWare
+export default authMiddleware
--- Backend/router/storyRouter.js
+++ Backend/router/storyRouter.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
 import {getStories,getStoryById,toggleBookmark} from "../controller/storyController.js"
-import authMiddleWare from "../middleware/authMiddleware.js"
+import authMiddleware from "../middleware/authMiddleware.js"
 
 const storyRouter = express.Router();
 
 storyRouter.get("/storyGet", getStories);
 storyRouter.get("/:id", getStoryById);
-storyRouter.post("/:id/bookmark", authMiddleWare, toggleBookmark);
+storyRouter.post("/:id/bookmark", authMiddleware, toggleBookmark);
 
 export default storyRouter;

724392b1d84a095b00a21d34d8f7625ec385cf1f
Arjun <arjunsinghanya35@gmail.com>
2026-05-17 22:30:20 +0530

    again try to deploye

--- Backend/router/storyRouter.js
+++ Backend/router/storyRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import {getStories,getStoryById,toggleBookmark} from "../controller/storyController.js"
-import authMiddleware from "../middleware/authMiddleware.js"
+import authMiddleware from "../middleware/authMiddleWare.js"
 
 const storyRouter = express.Router();
 
